e2f526ef3bb70641804346d8749ccb0467730ca9
fernando-candido <oficialfcsj@gmail.com>
2022-07-14 20:22:28 -0400

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
-  
-    return (
 
+  return (
+    <>
       <h1> Olá mundo </h1>
+      <NotificationButton />
+    </>
 
-    )
-  
+  )
+
 }
 
 export default App
